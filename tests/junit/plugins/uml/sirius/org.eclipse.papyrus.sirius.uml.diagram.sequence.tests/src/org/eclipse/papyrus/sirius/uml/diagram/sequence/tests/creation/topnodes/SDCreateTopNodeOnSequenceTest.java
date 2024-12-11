227227b75a574930ba8d923a198f069fb8772741
Nicolas PERANSIN <nicolas.peransin@obeo.fr>
2024-12-11 09:38:15 +0100

    GL-7 [Sirius][Sequence] Fix Lifeline creation test
    Creating a lifeline includes a end element for resize purpose.
    
    Bug: https://gitlab.eclipse.org/eclipse/papyrus/org.eclipse.papyrus-desktop/-/issues/7
    Signed-off-by: Nicolas PERANSIN <nicolas.peransin@obeo.fr>

--- tests/junit/plugins/uml/sirius/org.eclipse.papyrus.sirius.uml.diagram.sequence.tests/src/org/eclipse/papyrus/sirius/uml/diagram/sequence/tests/creation/topnodes/SDCreateTopNodeOnSequenceTest.java
+++ tests/junit/plugins/uml/sirius/org.eclipse.papyrus.sirius.uml.diagram.sequence.tests/src/org/eclipse/papyrus/sirius/uml/diagram/sequence/tests/creation/topnodes/SDCreateTopNodeOnSequenceTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.papyrus.junit.utils.rules.ActiveDiagram;
 import org.eclipse.papyrus.junit.utils.rules.PluginResource;
 import org.eclipse.papyrus.sirius.junit.util.diagram.AbstractCreateTopNodeOnDiagramTests;
@@ -35,6 +34,7 @@ import org.eclipse.uml2.uml.Constraint;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Interaction;
 import org.eclipse.uml2.uml.Lifeline;
+import org.eclipse.uml2.uml.Model;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -42,7 +42,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 /**
  * This class groups all tests about element creation on the root of the Sequence Diagram represented by an {@link Interaction}.
- * 
+ *
  * @author <a href="mailto:gwendal.daniel@obeosoft.com>Gwendal Daniel</a>
  */
 @PluginResource("resources/creation/topNodes/TopNode_CreationTest.di")
@@ -51,31 +51,26 @@ public class SDCreateTopNodeOnSequenceTest extends AbstractCreateTopNodeOnDiagra
 
 	protected static final String DIAGRAM_NAME = "TopNode_SequenceDiagramSirius"; //$NON-NLS-1$
 
-	private final String creationToolId;
-
-	private final String nodeMappingType;
-
-	private final EReference containmentFeature;
-
-	private final Class<? extends Element> expectedType;
+	private final NodeCreation usecase;
 
 	/**
 	 * Constructor.
+	 *
+	 * @param description
+	 *            test description
 	 */
-	public SDCreateTopNodeOnSequenceTest(Class<? extends Element> expectedType/* , EReference containmentFeature */) {
-		this.creationToolId = CreationToolsIds.getCreationToolId(expectedType);
-		this.nodeMappingType = MappingTypes.getMappingType(expectedType);
-		this.containmentFeature = ContainmentFeatureHelper.getContainmentFeature(expectedType);
-		this.expectedType = expectedType;
+	public SDCreateTopNodeOnSequenceTest(NodeCreation description) {
+		usecase = description;
+	}
+
+	private Interaction getMainInteraction() {
+		return (Interaction) ((Model) getSemanticOwner()).getMembers().get(0);
 	}
 
 	@Override
 	protected boolean applyCreationTool(String toolId, DDiagram diagram, EObject graphicalContainer) {
-		if (MappingTypes.getMappingType(Lifeline.class).equals(nodeMappingType)) {
-			/*
-			 * Lifeline tests require a custom tool creation invocation.
-			 */
-			return fixture.applyNodeCreationToolFromPalette(toolId, getDDiagram(), graphicalContainer, new Point(100, 100), null);
+		if (usecase.location != null) {
+			return fixture.applyNodeCreationToolFromPalette(toolId, diagram, graphicalContainer, usecase.location, null);
 		} else {
 			return super.applyCreationTool(toolId, diagram, graphicalContainer);
 		}
@@ -84,23 +79,61 @@ public class SDCreateTopNodeOnSequenceTest extends AbstractCreateTopNodeOnDiagra
 	@Test
 	@ActiveDiagram(DIAGRAM_NAME)
 	public void createTopNodeTest() {
-		final Diagram diagram = getDiagram();
-		final ISemanticRepresentationElementCreationChecker semanticChecker = new SemanticNodeCreationChecker(((org.eclipse.uml2.uml.Package) getSemanticOwner()).getPackagedElements().get(0), this.containmentFeature, this.expectedType);
-		DNodeCreationChecker graphicalNodeCreationChecker = new DNodeCreationChecker(diagram, getTopGraphicalContainer(), nodeMappingType);
-		if (MappingTypes.getMappingType(Lifeline.class).equals(nodeMappingType)) {
-			/*
-			 * Lifeline tests require a custom graphical checker: when a Lifeline is created, the header and the line are created.
-			 */
-			graphicalNodeCreationChecker.setExpectedCreatedElements(2);
-		}
-		/*
-		 * isSynchronize = true because Sequence diagrams have to be synchronized.
-		 */
-		createNode(creationToolId, new SemanticAndGraphicalCreationChecker(semanticChecker, graphicalNodeCreationChecker), getTopGraphicalContainer(), true);
+		final ISemanticRepresentationElementCreationChecker semanticChecker = new SemanticNodeCreationChecker(getMainInteraction(),
+				usecase.getContainment(), usecase.type);
+		DNodeCreationChecker viewChecker = new DNodeCreationChecker(getDiagram(), getTopGraphicalContainer(),
+				usecase.getMappingType());
+		viewChecker.setExpectedCreatedElements(usecase.createdNumber);
+
+		// isSynchronize = true because Sequence diagrams have to be synchronized.
+		createNode(usecase.getCreationTool(), new SemanticAndGraphicalCreationChecker(semanticChecker, viewChecker), getTopGraphicalContainer(), true);
 	}
 
+	/**
+	 * Creates Use-cases for tests.
+	 *
+	 * @return {@link NodeCreation}s
+	 */
 	@Parameters(name = "{index} Test {0} tool")
-	public static List<Class<? extends Element>> data() {
-		return List.of(Lifeline.class, Constraint.class, Comment.class);
+	public static List<NodeCreation> data() {
+		return List.of(
+				new NodeCreation(Lifeline.class, new Point(100, 100), 3 /* Header, body, resize footer */),
+				new NodeCreation(Constraint.class, null, 1),
+				new NodeCreation(Comment.class, null, 1));
 	}
+
+	/**
+	 * Use-case for test suite.
+	 * <p>
+	 * Provides the parameters for a test instance.
+	 * </p>
+	 *
+	 * @param type
+	 *            class of element to create
+	 * @param location
+	 *            indicates where the creation should happen
+	 * @param createdNumber
+	 *            number of created elements
+	 *
+	 */
+	record NodeCreation(Class<? extends Element> type, Point location, int createdNumber) {
+
+		String getCreationTool() {
+			return CreationToolsIds.getCreationToolId(type);
+		}
+
+		String getMappingType() {
+			return MappingTypes.getMappingType(type);
+		}
+
+		EReference getContainment() {
+			return ContainmentFeatureHelper.getContainmentFeature(type);
+		}
+
+		@Override
+		public final String toString() {
+			return getCreationTool();
+		}
+	}
+
 }
